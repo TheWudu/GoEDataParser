e390e074b49de6710824271ebe16b108733f6de0
maf <maf@runtastic.com>
2025-01-30 10:38:36 +0100

    Improve CsvParser tests

--- GoEDataParserTest/CsvParserTest.cs
+++ GoEDataParserTest/CsvParserTest.cs
@@ -4,21 +4,38 @@ public class CsvParserTests
 {
     string appPath = System.AppDomain.CurrentDomain.BaseDirectory;
 
-    List<Charging.Charge> charges;
+    List<Charging.Charge> charges = [];
+    Charging.CsvParser parser;
 
     [SetUp]
     public void Setup()
     {
-        string filepath = appPath + "/../../../fixtures/20250124_goe.csv";
-        Charging.CsvParser parser = new();
-        parser.parse(filepath);
+        parser = new();
+    }
 
+    public void Initialize(string filename)
+    {
+        string filepath = appPath + "/../../../fixtures/" + filename;
+
+        parser.parse(filepath);
         charges = parser.GetCharges();
     }
 
-    [Test]
-    public void CsvParserTest1()
+    [TestCase("no_entry.csv", 0)]
+    [TestCase("one_entry.csv", 1)]
+    [TestCase("20250124_goe.csv", 256)]
+    public void CountRowsTest(string filename, int expected_count)
     {
+        Initialize(filename);
+
+        Assert.That(charges, Has.Count.EqualTo(expected_count));
+    }
+
+    [Test]
+    public void CsvParserTestRealFile()
+    {
+        Initialize("20250124_goe.csv");
+
         Assert.Multiple(() =>
         {
             Assert.That(charges[0].kwh, Is.EqualTo(0.071F));
